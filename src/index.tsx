0a227374a5a69e2fde05a2ec700ed17e2f59980d
VPagent <vpagent13@gmail.com>
2022-11-29 21:55:15 +0200

    route

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/test-task-finish'>
+    <BrowserRouter basename='test-task-finish'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
